489fae7fe53be83f38c492a4d592e75c62d697fe
Jesus Alejandro Artiaga Morales <jesusartiaga.chucho@gmail.com>
2024-10-09 11:19:21 -0600

    Practica 7
    Arreglos: Agregacion de ejercicios con operador ternario y switch - case

--- JS/Ejercicio_05.js
+++ JS/Ejercicio_05.js
@@ -6,7 +6,7 @@ const style_console = `background: ${bg}; color: white; border-radius: 6px; padd
 
 console.warn("Práctica 07: Arreglos en JavaScript")
 
-console.log("%c1.- Si/Entoces ... (IF)", style_console)
+console.log("%c1.- Si/Entoces (IF)", style_console)
 
 //Le indica al programa que hacer o que no en base a una prueba lógica de verdadero o falso 
 let fechaActual=new Date();
@@ -41,21 +41,93 @@ else
 
 //que pasa si la validación tiene varias operaciones 
 const anio= fechaActual.getFullYear()
-let inicioPrimavera= new Date(anio, 2, 21);
-let inicioVerano= new Date(anio, 5, 21);
-let inicioOtonio= new Date(anio, 8, 23);
-let inicioInvierno= new Date(anio, 11, 21);
+let inicioPrimavera = new Date(anio, 2, 21);
+let inicioVerano = new Date(anio, 5, 21);
+let inicioOtonio = new Date(anio, 8, 23);
+let inicioInvierno = new Date(anio, 11, 21);
 
-let horarioVerano= false;
+let horarioVerano = false;
 
-if(fechaActual>= inicioPrimavera || fechaActual< inicioVerano) {
+if(fechaActual >= inicioPrimavera && fechaActual < inicioVerano) {
     console.log("Estamos en PRIMAVERA...")
     console.log("Inicia la floracion de muchas plantas...");
     console.log("Los dias son mas largos y las noches mas cortas...")
     console.log("Muchos animales despiertan de la hibernacion...")
     estacion = "Primavera"
     horarioVerano = true
-} else if(fechaActual >= inicioVerano && fechaActual < inicioOtonio){
+} 
+else if(fechaActual >= inicioVerano && fechaActual < inicioOtonio){
+    console.log("Estamos en VERANO...")
+    console.log("En esta temporada abundan los dias soleados y calurosos...")
+    console.log("En esta temporada el indice de turismo vacacional sube...")
+    console.log("Mucha gente busca realizar actividades al aire...")
+    estacion = "Verano"
+    horarioVerano = true
 
+} 
+else if(fechaActual >= inicioOtonio && fechaActual < inicioInvierno){
+    console.log("Estamos en OTOÑO...")
+    console.log("Los arboles suelen camviar de follaje...")
+    console.log("Se registran temperaturas mas templadas...")
+    console.log("Los animales comienzan con la recoleccion de alimento y preparan sus espacios para la hibernacion, incluso algunas aves migran...")
+    estacion = "Otoño"
+    horarioVerano = false
+}
+else {
+    console.log("Estamos en INVIERNO...")
+    console.log("En esta temporada los dias son mas cortos y las noches mas largas...")
+    console.log("En algunas regiones suele nevar...")
+    console.log("Dado las bajas temperaturas, se recomienda abrigarse...")
+    estacion = "Invierno"
+    horarioVerano = false
+}
+
+
+console.log("%c2.- Operador Ternario (validacion ? cumple : no_cumple)", style_console)
+//En JS existe una operacion simplificada que valida si una condicion se comple o no, y que hacer en cada caso
+
+const edadPersona = 20
+const mayoriaEdadMX = 18
+const mayoriaEdadUS = 21
+
+let evaluarMayoriaEdad = (edad) => 
+    edad >= 18 ? "Eres mayor de edad." : "No eres mayor de edad."  //Operacion Ternaria
+
+console.log("Evaluando la mayoria de edad de la persona...")
+console.log(evaluarMayoriaEdad(edadPersona))
+
+//Sin embargo tenemos que considerar que la mayoria de edad varia en cada pais por cuestiones legales, por lo que debemos cosiderar un segundo parametro en la evaluacion
+
+evaluarMayoriaEdad = (edad, pais) =>
+    (edad >= 18 && pais == "MX") ? `En ${pais} eres mayor de edad ` : `En ${pais} no eres mayor de edad`
+
+console.log("Evaluando la mayoria de edad de una persona en Mexico...")
+console.log(evaluarMayoriaEdad(edadPersona, "MX"))
+
+console.log("Evaluando la mayoria de edad de una persona en Estados Unidos...")
+console.log(evaluarMayoriaEdad(edadPersona, "US"))
+
+
+console.log("%c3.- SWITCH - CASE (Eleccion por valor definido)", style_console)
+//Calculando tu generacion en base a tu edad
+
+let asignageneracion = (aniNacimiento) => {
+    switch(true){
+        
+        case(aniNacimiento < 1968) :
+        return "Baby Boomers"
+
+        case(aniNacimiento > 1968 && aniNacimiento <= 1980) :
+        return "Generacion X"
+
+        case(aniNacimiento > 1980 && aniNacimiento <= 1994) :
+        return "Milenials"
+
+        case(aniNacimiento > 1994 && aniNacimiento <= 2010) :
+        return "Generacion X"
+
+        case(aniNacimiento > 2010) :
+        return "Krystal"
+    }
 }
 
